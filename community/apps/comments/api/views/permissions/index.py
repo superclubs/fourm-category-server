b17df2e3c00d0686b59dd21d97369549849e2888
andyingi <andy@runners.im>
2023-11-30 16:57:34 +0900

    고침: 오타 고침

--- community/apps/comments/api/views/permissions/index.py
+++ community/apps/comments/api/views/permissions/index.py
@@ -5,9 +5,7 @@ from rest_framework.permissions import BasePermission
 # Main Section
 class CommentPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.club:
-            if view.action in ['comment_report', 'comment_comment']:
-                if request.user.id is None:
-                    return False
-
+        if view.action in ['comment_report', 'comment_comment']:
+            if request.user.id is None:
+                return False
         return True
