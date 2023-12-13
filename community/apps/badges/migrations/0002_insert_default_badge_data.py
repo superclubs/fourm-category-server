cd2e24909fbf0957a2efa8656ca304a4d5e78694
andyingi <andy@runners.im>
2023-12-13 11:56:47 +0900

    고침: 디폴트 뱃지 마이그레이션 파일 고침

--- community/apps/badges/migrations/0002_insert_default_badge_data.py
+++ community/apps/badges/migrations/0002_insert_default_badge_data.py
@@ -7,130 +7,130 @@ from django.conf import settings
 def forwards_insert_default_badge_data(apps, schema_editor):
     Badge = apps.get_model('badges', 'Badge')
 
-    SERVICE_TITLE = settings.SERVICE_TITLE.lower()
+    SERVICE_PATH = settings.SERVICE_PATH
     MEDIA_URL = settings.MEDIA_URL
 
     badge_list = [
         {
             'title': 'New',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-new%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-new%403x.png'
         },
         {
             'title': 'Nice',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-nice%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-nice%403x.png'
         },
         {
             'title': 'Good',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-good%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-good%403x.png'
         },
         {
             'title': 'Excellent',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-excellent%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-excellent%403x.png'
         },
         {
             'title': 'Great',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-great%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-great%403x.png'
         },
         {
             'title': 'Wonderful',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-wonderful%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-wonderful%403x.png'
         },
         {
             'title': 'Fantastic',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-fantastic%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-fantastic%403x.png'
         },
         {
             'title': 'Amazing',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-amazing%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-amazing%403x.png'
         },
         {
             'title': 'Live Best',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-best-live%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-best-live%403x.png'
         },
         {
             'title': 'Weekly Best',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-best-weekly%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-best-weekly%403x.png'
         },
         {
             'title': 'Monthly Best',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-best-monthly%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-best-monthly%403x.png'
         },
         {
             'title': 'Rising COMMUNITY',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-rising-forum%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-rising-forum%403x.png'
         },
         {
             'title': 'Recommend COMMUNITY',
             'model_type': 'COMMUNITY',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-forum-recommend-forum%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-forum-recommend-forum%403x.png'
         },
         {
             'title': 'New',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-new%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-new%403x.png'
         },
         {
             'title': 'Nice',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-nice%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-nice%403x.png'
         },
         {
             'title': 'Good',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-good%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-good%403x.png'
         },
         {
             'title': 'Excellent',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-excellent%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-excellent%403x.png'
         },
         {
             'title': 'Great',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-great%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-great%403x.png'
         },
         {
             'title': 'Wonderful',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-wonderful%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-wonderful%403x.png'
         },
         {
             'title': 'Fantastic',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-fantastic%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-fantastic%403x.png'
         },
         {
             'title': 'Amazing',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-amazing%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-amazing%403x.png'
         },
 
         {
             'title': 'Live Best',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-best-live%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-best-live%403x.png'
         },
         {
             'title': 'Weekly Best',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-best-weekly%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-best-weekly%403x.png'
         },
         {
             'title': 'Monthly Best',
             'model_type': 'POST',
-            'url': f'{MEDIA_URL}{SERVICE_TITLE}/badge/default/badge-post-best-monthly%403x.png'
+            'url': f'{MEDIA_URL}{SERVICE_PATH}/badge/default/badge-post-best-monthly%403x.png'
         }
     ]
 
