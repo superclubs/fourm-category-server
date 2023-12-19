84cbe13d8fb6ad1ab38d97308efd1749ddd7a3df
Leo <leoyunhyung@gmail.com>
2023-12-19 23:09:02 +0900

    추가: 커뮤니티 미디어 시리얼라이저

---
+++ community/apps/community_medias/api/serializers/index.py
@@ -0,0 +1,17 @@
+# Bases
+from community.bases.api.serializers import ModelSerializer
+
+# Utils
+from community.utils.api.fields import HybridImageField
+
+# Models
+from community.apps.community_medias.models import CommunityMedia
+
+
+# Main Section
+class CommunityMediaSerializer(ModelSerializer):
+    file = HybridImageField(use_url=True, required=False)
+
+    class Meta:
+        model = CommunityMedia
+        fields = ('file', 'media_type', 'file_type', 'order')
