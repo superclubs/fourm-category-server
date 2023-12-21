b5fd9f03040f1af56dfaa1c9fd1283993f889a92
Leo <leoyunhyung@gmail.com>
2023-12-21 16:22:53 +0900

    변경: 매니징 유무 필드명

--- community/apps/communities/api/serializers/retrieve.py
+++ community/apps/communities/api/serializers/retrieve.py
@@ -13,13 +13,13 @@ class CommunityRetrieveSerializer(ModelSerializer):
     boards = serializers.JSONField(source='board_data')
     posts = serializers.JSONField(source='posts_data')
     banner_medias = serializers.JSONField(source='banner_medias_data')
-    is_manager = serializers.SerializerMethodField()
+    is_managing = serializers.SerializerMethodField()
 
     class Meta:
         model = Community
-        fields = ('id', 'title', 'is_manager', 'banner_medias', 'posts', 'boards')
+        fields = ('id', 'title', 'is_managing', 'banner_medias', 'posts', 'boards')
 
-    def get_is_manager(self, obj):
+    def get_is_managing(self, obj):
         request = self.context.get('request', None)
         if not request:
             return None
@@ -28,5 +28,5 @@ class CommunityRetrieveSerializer(ModelSerializer):
             return None
         community_user = obj.community_users.filter(user=user, is_active=True).first()
         if not community_user:
-            return True
-        return False
+            return False
+        return True
