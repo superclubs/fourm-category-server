cea17f37525c1d9d0f79ab51b32f2fda3b623960
Leo <leoyunhyung@gmail.com>
2023-12-21 15:46:47 +0900

    변경: 커뮤니티 객체 조회 시, 매니저 여부 필드

--- community/apps/communities/api/serializers/retrieve.py
+++ community/apps/communities/api/serializers/retrieve.py
@@ -13,7 +13,20 @@ class CommunityRetrieveSerializer(ModelSerializer):
     boards = serializers.JSONField(source='board_data')
     posts = serializers.JSONField(source='posts_data')
     banner_medias = serializers.JSONField(source='banner_medias_data')
+    is_manager = serializers.SerializerMethodField()
 
     class Meta:
         model = Community
-        fields = ('id', 'title', 'banner_medias', 'posts', 'boards')
+        fields = ('id', 'title', 'is_manager', 'banner_medias', 'posts', 'boards')
+
+    def get_is_manager(self, obj):
+        request = self.context.get('request', None)
+        if not request:
+            return None
+        user = request.user
+        if not user.id:
+            return None
+        community_user = obj.community_users.filter(user=user, is_active=True).first()
+        if not community_user:
+            return True
+        return False
