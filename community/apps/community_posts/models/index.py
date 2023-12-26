7f5357cbb8087b33374c9e271629dd476eba121d
Leo <leoyunhyung@gmail.com>
2023-12-26 19:19:46 +0900

    삭제: 커뮤니티 포스트 테이블

--- community/apps/community_posts/models/index.py
+++ community/apps/community_posts/models/index.py
@@ -7,18 +7,4 @@ from community.bases.models import Model
 
 
 # Main Section
-class CommunityPost(Model):
-    # Fk
-    community = models.ForeignKey('communities.Community', verbose_name=_('Community'), on_delete=models.CASCADE,
-                                  related_name='community_posts')
-    post = models.ForeignKey('posts.Post', verbose_name=_('Post'), on_delete=models.CASCADE,
-                             related_name='community_posts')
-    # Main
-    order = models.IntegerField(_('Order'), default=1)
 
-    # Data
-    post_data = models.JSONField(_('Post Data'), null=True, blank=True)
-
-    class Meta:
-        verbose_name = verbose_name_plural = _('Community Post')  # 커뮤니티 에디터픽 포스트
-        ordering = ['order']
