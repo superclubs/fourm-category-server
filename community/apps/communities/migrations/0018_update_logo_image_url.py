50b0be9ca0213fc19259ad2fe6cd6caeac46d63a
Andy <andy@runners.im>
2024-06-10 16:27:35 +0900

    고침: 커뮤니티 로고 이미지 url 변경

---
+++ community/apps/communities/migrations/0018_update_logo_image_url.py
@@ -0,0 +1,31 @@
+# Django
+from django.db import migrations
+
+
+# Main Section
+def forwards_update_logo_image_url(apps, schema_editor):
+    Community = apps.get_model('communities', 'Community')
+    communities = Community.objects.all()
+
+    for community in communities:
+        community.logo_image_url = community.logo_image_url.replace('svg', 'png')
+        community.save()
+
+    return True
+
+
+def reverse_update_logo_image_url(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('communities', '0017_update_categories'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            code=forwards_update_logo_image_url,
+            reverse_code=reverse_update_logo_image_url,
+        ),
+    ]
